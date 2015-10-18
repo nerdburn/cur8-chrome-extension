683083be8786c1072794add197e1ebb4d839d795
Shawn Adrian <shawn@nerdburn.com>
2015-10-17 22:22:29 -0700

    making some style / functionality changes to log in and save site

--- popup.js
+++ popup.js
@@ -1,138 +1,151 @@
-//chrome apis
+// chrome apis
 var t = chrome.tabs;
 
-//page elaments
-var addForm = $("#addsite"),
+// develop mode
+var develop = true;
+var hostname = (develop)? 'localhost:3000' : 'cur8.io';
+
+var siteName = $('.site-name');
+var saveBtn = $('.save-btn');
+var searchInput = $('.srch-term');
+
+//p age elaments
+var addForm = $(".add-site"),
     saveAddBtn = $("#submitSave"),
-    loginForm = $("#login"),
+    loginForm = $(".log-in"),
     emailIpt = $("#email"),
     passwordIpt = $("#password"),
     loginBtn = $("#loginBtn"),
     statusMsg = $("#status-display"),
-    savedInfo = $("#saveSuccess"),
+    savedInfo = $(".save-success"),
     messageLabel = $(".message"),
     curDomain = "",
     curUrl = "",
     searchInput = $("#searchSites"),
     searchBtn = $("#searchIcon");
 
-searchInput.keypress(function (e) {
-    if (e.keyCode === 13) {
-        queryWord();
-    }
+// search
+searchInput.keypress(function(e) {
+  if(e.keyCode === 13) {
+    queryWord();
+  }
 });
 
-searchBtn.click(function (e) {
-    queryWord();
-});
 function queryWord() {
-    var query = searchInput.val().trim();
-    if (query != "") {
-        ajaxCall(
-            "http://cur8.io/api/search/" + localStorage.token,
-            {terms: query},
-            "POST",
-            "json",
-            function (data) {
-                if (data && data.search_url) {
-                    window.open(data.search_url);
-                } else {
-                    if (data.message) {
-                        alert(data.message);
-                    } else {
-                        alert("something is wrong");
-                    }
-                }
-            },
-            handleAjaxError
-        );
-    }
+  var query = searchInput.val().trim();
+  if(query != '') {
+    ajaxCall(
+      'http://'+hostname+'/api/search/' + localStorage.token, { terms: query }, 'POST', 'json', function(data) {
+        if(data && data.search_url) {
+          window.open(data.search_url);
+        } else {
+          if(data.message) {
+            alert(data.message);
+          } else {
+            alert("something is wrong");
+          }
+        }
+      },
+      handleAjaxError
+    );
+  }
 }
 
-//get current tab
-t.query({currentWindow: true, active: true}, function (tabs) {
-    var tab = tabs[0];
-    if (!validUrl(tab.url)) {
-        statusMsg.text("you are not on a valid page");
+// on page load - get current tab
+t.query({ currentWindow: true, active: true }, function(tabs) {
+  var tab = tabs[0];
+  if(!validUrl(tab.url)) {
+    console.log('This page cannot be saved.');
+  } else {
+    curDomain = getDomain(tab.url);
+    curUrl = tab.url;
+    
+    //localStorage.removeItem('token');
+    
+    // check if logged in
+    if(localStorage.token) {
+      
+      // hide log in form if logged in
+      loginForm.hide();
+      console.log('is logged in');
+      
+      // check if current url is already saved
+      if(isInLocalStorage('savedSites', curDomain)) {
+        console.log('domain has been saved');
+        siteName.text('Site ' + curDomain + ' is already saved.');
+        saveBtn.hide();
+      } else {
+        siteName.text(curDomain);
+        saveBtn.show();
+      }
+      
+      addForm.show();
+      searchInput.focus();
     } else {
-        curDomain = getDomain(tab.url);
-        curUrl = tab.url;
-        messageLabel.text(curDomain);
-        //check if token stored
-        if (localStorage.token) {
-            if (isInLocalStorage("savedSites", curDomain)) {
-                savedInfo.show().find("h2").text("Already Saved");
-            } else {
-                $(".header").hide();
-                $(".siteName").text(curDomain);
-                addForm.show();
-            }
-
-        } else {
-            loginForm.show();
-        }
+      addForm.hide();
+      loginForm.show();
     }
+  }
 });
 
 saveAddBtn.click(addSite);
 loginBtn.click(logIn);
 
-
 function getDomain(url) {
-    try {
-        return (new URL(url)).hostname;
-    } catch (e) {
-        return null;
-    }
+  try {
+    return (new URL(url)).hostname;
+  } catch(e) {
+    return null;
+  }
 }
 
 // POST the data to the server using XMLHttpRequest
 function addSite() {
-    statusMsg.text('Saving...');
-    var url = 'http://cur8.io/api/' + localStorage.token;
-    var params = {url: curUrl, uid: localStorage.uid};
-    ajaxCall(url, params, "POST", "json", function (data) {
-        if (data && data.message == "Saved!") {
-            saveArrayInLocalStorage("savedSites", curDomain);
-            statusMsg.hide();
-            addForm.slideUp("fast");
-            $("#saveSuccess").slideDown();
-        } else {
-            statusMsg.text("error, please try again later");
-        }
-    }, handleAjaxError);
-
+  var url = 'http://'+hostname+'/api/' + localStorage.token;
+  var params = { url: curUrl, uid: localStorage.uid };
+  ajaxCall(url, params, 'POST', 'json', function(data) {
+    if(data && data.message == 'Saved!') {
+      saveArrayInLocalStorage('savedSites', curDomain);
+      siteName.text('Site ' + curDomain + ' saved!');
+      saveBtn.hide();
+    } else {
+      siteName.text('Error saving...');
+    }
+  }, handleAjaxError);
 }
+
+// return false on form login
+$('form.log-in').submit(function(){
+  return false;
+});
 
 // POST the data to the server using XMLHttpRequest
 function logIn() {
-    statusMsg.text("log in...");
-    var url = 'http://cur8.io/api/login';
-    var params = getEmailAndPassword();
-    ajaxCall(url, params, "POST", "json", function (data) {
-        if (data.loginToken) {
-            console.log('token found: ', data.loginToken);
-            localStorage.token = data.loginToken;
-            localStorage.uid = data.userId;
-            $('#login').hide();
-            $('#addsite').show();
-            statusMsg.text('');
-        }
-    }, handleAjaxError);
+  var url = 'http://'+hostname+'/api/login';
+  var params = getEmailAndPassword();
+  console.log('params: ', params);
+  ajaxCall(url, params, 'POST', 'json', function(data) {
+    console.log('data returned: ', data);
+    if(data.loginToken) {
+      console.log('token found: ', data.loginToken);
+      localStorage.token = data.loginToken;
+      localStorage.uid = data.userId;
+      loginForm.hide();
+      addForm.show();
+    }
+  }, handleAjaxError);
 }
 
 function getEmailAndPassword() {
-    var email = emailIpt.val().trim(),
-        password = passwordIpt.val().trim();
-    if (email == "" || password == "" && !validateEmail(email)) {
-        alert("input valid things");
-    } else {
-        return {email: email, password: password};
-    }
+  var email = emailIpt.val().trim();
+  var password = passwordIpt.val().trim();
+  if(email == "" || password == "" && !validateEmail(email)) {
+    console.log('Invalid credentials.');
+  } else {
+    return { email: email, password: password };
+  }
 }
 
 function handleAjaxError(msg) {
-    console.log(msg);
+  console.log(msg);
 }
-
-
